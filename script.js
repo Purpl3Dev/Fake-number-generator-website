c035d8b9a6d7cefa764e7e64a3c24b2c61f10dc1
Purpl3Dev <GreenedPurple@protonmail.com>
2022-01-29 17:28:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function generateRandomNum() {
   const toNum = document.querySelector(".toNum").value;
 
   randomNum = Math.floor(Math.random() * parseInt(toNum));
-  if(randomNum==7) randomNum = 8;
-  if(randomNum==3) randomNum = 4;
-  if(randomNum==20) randomNum = 21;
+  
+  if(randomNum==7) randomNum = 8; /* Put here your number to exclude in brackets and number to trasform in*/
+  if(randomNum==3) randomNum = 4; /* Put here your number to exclude in brackets and number to trasform in*/
+  
   if (randomNum >= parseInt(fromNum)) {
     const randomNumEl = document.querySelector(".randomNumEl");
     randomNumEl.innerHTML = randomNum;
